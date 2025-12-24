1ceb3e776a69cbb189ba280c8b4aad9b8b77c80c
Syedharris726 <81823363+Syedharris726@users.noreply.github.com>
2025-12-24 19:32:16 +0530

    Create stringbits.java

---
+++ Warmup-2/stringbits.java
@@ -0,0 +1,14 @@
+/*
+  Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+  
+  
+  stringBits("Hello") → "Hlo"
+  stringBits("Hi") → "H"
+  stringBits("Heeololeo") → "Hello"
+*/
+public String stringBits(String str) {
+  String a="";
+  for(int i=0; i<str.length(); i+=2)
+    a+=str.charAt(i);
+  return a;
+}
