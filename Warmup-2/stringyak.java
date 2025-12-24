493a905b7ba8cdea48c9d033bd6d1e07b1e5260e
Syedharris726 <81823363+Syedharris726@users.noreply.github.com>
2025-12-24 20:50:32 +0530

    Create stringyak.java

---
+++ Warmup-2/stringyak.java
@@ -0,0 +1,18 @@
+/*
+  Suppose the string "yak" is unlucky. Given a string, return a version where all the "yak" are removed, but the "a" can be any char. The "yak" strings will not overlap.
+  
+  stringYak("yakpak") → "pak"
+  stringYak("pakyak") → "pak"
+  stringYak("yak123ya") → "123ya"
+*/
+public String stringYak(String str) {
+  String a="";
+  for(int i=0;i<str.length();i++){
+    if (i+2<str.length() && str.substring(i,i+3).equals("yak")) 
+      i =  i + 2;
+    else
+      a+=str.charAt(i);
+      
+  }
+  return a;
+}
